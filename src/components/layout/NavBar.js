15dc7bfacfccd1b2c64c61b4209db79b835b93f3
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-23 15:26:18 +1000

    User data passed through

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,26 +1,34 @@
 import React from "react"
-import PropTypes from 'prop-types'
-import logo from './GithubFinderLogoWhite.png'
+import PropTypes from "prop-types"
+import logo from "./GithubFinderLogoWhite.png"
 
-const NavBar = ({title}) => {
-  
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <img src={logo} alt="Logo" style={{height: "20px", width: "auto"}} />
-          {title}
-        </h1>
-      </nav>
-    )
+const NavBar = ({ title }) => {
+  return (
+    <div>
+        <nav
+          className='navbar bg-primary'
+          style={{ position: "fixed", top: 0, margin: "0 0 50px 0" }}
+        >
+          <h1>
+            <img
+              src={logo}
+              alt='Logo'
+              style={{ height: "20px", width: "auto" }}
+            />
+            {title}
+          </h1>
+        </nav>
+      <div style={{ position: "relative", top: 0, margin: "0 0 80px 0" }}></div>
+    </div>
+  )
 }
 
 NavBar.defaultProps = {
-    title: ' Github Finder'
+  title: " Github Finder"
 }
 
 NavBar.propTypes = {
-title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 }
-
 
 export default NavBar
