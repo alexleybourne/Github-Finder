e4f5e00baef2c3b3c3e417f1210fe16475d08d9c
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-23 12:14:17 +1000

    Refactoring classes to functions

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from 'prop-types'
 import logo from './GithubFinderLogoWhite.png'
 
-export class NavBar extends Component {
-  static defaultProps = {
-    title: ' Github Finder'
-  }
-
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  }
-
-  render() {
+const NavBar = ({title}) => {
+  
     return (
       <nav className='navbar bg-primary'>
         <h1>
           <img src={logo} alt="Logo" style={{height: "20px", width: "auto"}} />
-          {this.props.title}
+          {title}
         </h1>
       </nav>
     )
-  }
 }
+
+NavBar.defaultProps = {
+    title: ' Github Finder'
+}
+
+NavBar.propTypes = {
+title: PropTypes.string.isRequired,
+}
+
 
 export default NavBar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react"
+import React from "react"
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-  render() { 
-
-    const { login, avatar_url, html_url} = this.props.user
-
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
+  
     return (
       <div className='card text-center'>
         <img
@@ -15,11 +13,14 @@ class UserItem extends Component {
         />
         <h3>{login}</h3>
         <div>
-            <a href={html_url} className="btn btn-dark btn-sm my-1" target="_blank">More</a>
+            <a href={html_url} className="btn btn-dark btn-sm my-1" target="_blank" rel="noopener noreferrer" >More</a>
         </div>
       </div>
     )
-  }
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem
