d493e14b175a03483e6eb21bfc38bc75b013fffd
alexleybourne <alexleybourne1@gmail.com>
2020-01-26 13:21:00 +1000

    Path to about added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NavBar from "./components/layout/NavBar"
 import Users from "./components/users/Users"
 import Search from "./components/users/Search"
 import Alert from "./components/layout/Alert"
+import About from "./components/pages/About"
 import axios from "axios"
 
 class App extends Component {
@@ -67,6 +68,7 @@ class App extends Component {
               <Users loading={loading} users={users} />
             </Fragment>
           } />
+          <Route exact path = "/about" component = {About}/>
         </Switch>
           
         </div>
