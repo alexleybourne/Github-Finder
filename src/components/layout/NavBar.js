d2ac7d2981e7799d615796d38c0333130eead0ce
alexleybourne <alexleybourne1@gmail.com>
2020-01-26 13:27:16 +1000

    Links added to navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import logo from "./GithubFinderLogoWhite.png"
+import { Link } from "react-router-dom"
 
 const NavBar = ({ title }) => {
   return (
@@ -17,6 +18,14 @@ const NavBar = ({ title }) => {
             />
             {title}
           </h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
         </nav>
       <div style={{ position: "relative", top: 0, margin: "0 0 80px 0" }}></div>
     </div>
